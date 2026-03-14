c29cdc31d4d6ef5a926b5780aadefcafdbf2bf9d
JephEbuka <jaguarjeph@gmail.com>
2026-03-14 18:31:07 +0100

    Working

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char tmp;
+
+	while (s[j] != '\0')
+		j++;
+
+	j = j - 1;
+
+	while (i < j)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i++;
+		j--;
+	}
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x05-pointers_arrays_strings
+
+Pointer, array, and string practice in C: updating ints via pointers, swapping values, custom string length and copy routines, and printing strings or parts of strings using `_putchar` and `printf` as required. [page:8]
